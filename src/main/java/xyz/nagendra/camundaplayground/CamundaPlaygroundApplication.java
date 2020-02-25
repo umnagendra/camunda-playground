55b5dfe4621eb8b54935672dfb8b6d6348326e26
Nagendra Mahesh <namahesh@cisco.com>
2020-02-25 18:55:49 +0530

    Spring Boot skeleton

---
+++ src/main/java/xyz/nagendra/camundaplayground/CamundaPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package xyz.nagendra.camundaplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/nagendra/camundaplayground/CamundaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.nagendra.camundaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
