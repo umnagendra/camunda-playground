fa1f3873a866a9483fc6e447f0aac3260c140609
Nagendra Mahesh <namahesh@cisco.com>
2020-02-25 19:50:25 +0530

    Initialized camunda bpm with MYSQL and spring data JPA

--- src/main/java/xyz/nagendra/camundaplayground/CamundaPlaygroundApplication.java
+++ src/main/java/xyz/nagendra/camundaplayground/CamundaPlaygroundApplication.java
@@ -1,9 +1,11 @@
 package xyz.nagendra.camundaplayground;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class CamundaPlaygroundApplication {
 
 	public static void main(String[] args) {
