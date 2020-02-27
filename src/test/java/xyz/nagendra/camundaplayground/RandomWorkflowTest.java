c062c804a44ccf5efbc55f3616fd47cf997fb51a
Nagendra Mahesh <namahesh@cisco.com>
2020-02-27 16:12:16 +0530

    Fixed assertion for completed process instance

--- src/test/java/xyz/nagendra/camundaplayground/RandomWorkflowTest.java
+++ src/test/java/xyz/nagendra/camundaplayground/RandomWorkflowTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -22,9 +23,19 @@ public class RandomWorkflowTest {
     @Test
     public void startRandomWorkflow() throws InterruptedException {
         ProcessInstance pi = runtimeService.startProcessInstanceByKey("RandomWorkflow");
+
         assertNotNull(pi);
+        String processInstanceId = pi.getProcessInstanceId();
+
         // wait for some time so the process instance can complete
-        Thread.sleep(3000);
-        assertTrue(pi.isEnded());
+        // No idea why, but we've to wait for a long time to ensure the process is ended and flushed
+        Thread.sleep(20000);
+
+        // Ref. https://groups.google.com/forum/#!topic/camunda-bpm-users/d0qntahqqXI
+        // If a process instance runs through till the end it is removed from the runtime database
+        // and only exists in the history database afterwards.
+        // So you have to query the process instance from the history to check if it is ended
+        // (or check if a query on the runtime returns no result).
+        assertNull(runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult());
     }
 }
