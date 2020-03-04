6a56c0640e5bfddee741ebae64ed9c5686899a02
Nagendra Mahesh <namahesh@cisco.com>
2020-03-04 22:39:04 +0530

    Misc.

--- src/main/java/xyz/nagendra/camundaplayground/Constants.java
+++ src/main/java/xyz/nagendra/camundaplayground/Constants.java
@@ -9,6 +9,8 @@ public class Constants {
     public static final String VAR_NAME_NUM = "num";
     public static final String VAR_NAME_IS_EVEN = "isEven";
     public static final String VAR_NAME_APPROVED_BY = "approvedBy";
+    public static final String VAR_NAME_ERROR_OCCURED = "ERROR-OCCURED";
+    public static final String VAR_NAME_ERROR_MSG = "ERROR-MSG";
 
     public static final String TASK_ID_WAIT_APPROVAL = "Task_Wait_Approval";
 }
--- src/main/java/xyz/nagendra/camundaplayground/tasks/randomworkflow/ErrorHandlingDelegate.java
+++ src/main/java/xyz/nagendra/camundaplayground/tasks/randomworkflow/ErrorHandlingDelegate.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import static xyz.nagendra.camundaplayground.Constants.VAR_NAME_ERROR_MSG;
+import static xyz.nagendra.camundaplayground.Constants.VAR_NAME_ERROR_OCCURED;
 import static xyz.nagendra.camundaplayground.Constants.VAR_NAME_STARTED_AT;
 import static xyz.nagendra.camundaplayground.Constants.VAR_NAME_STARTED_BY;
 
@@ -18,6 +20,6 @@ public class ErrorHandlingDelegate implements JavaDelegate {
     public void execute(DelegateExecution execution) throws Exception {
         LOGGER.info("Inside ErrorHandlingDelegate ... started by = {} @ {}",
                 execution.getVariable(VAR_NAME_STARTED_BY), execution.getVariable(VAR_NAME_STARTED_AT));
-        LOGGER.info("Error occurred. Code = {}, Message = {}", execution.getVariable("ERROR-OCCURED"), execution.getVariable("ERROR-MSG"));
+        LOGGER.info("Error occurred. Code = {}, Message = {}", execution.getVariable(VAR_NAME_ERROR_OCCURED), execution.getVariable(VAR_NAME_ERROR_MSG));
     }
 }
